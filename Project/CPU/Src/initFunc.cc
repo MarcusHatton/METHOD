3cc54a8154ae5e2666934bc3318503b289f68fc6
Marcus J Hatton <mjh1n20@soton.ac.uk>
2021-06-24 16:37:51 +0100

    FIX on z-value limits in shocktube1D

--- Project/CPU/Src/initFunc.cc
+++ Project/CPU/Src/initFunc.cc
@@ -1257,7 +1257,7 @@ IS_Shocktube_1D::IS_Shocktube_1D(Data * data, int dir) : InitialFunc(data)
   // Limit checking
   if ((d->xmin != 0.0 || d->xmax != 1.0) && dir==0) throw std::invalid_argument("Domain has incorrect values. Expected x E [0.0, 1.0]\n");
   if ((d->ymin != 0.0 || d->ymax != 1.0) && dir==1) throw std::invalid_argument("Domain has incorrect values. Expected y E [0.0, 1.0]\n"); 
-  if ((d->ymin != 0.0 || d->ymax != 1.0) && dir==2) throw std::invalid_argument("Domain has incorrect values. Expected y E [0.0, 1.0]\n"); 
+  if ((d->zmin != 0.0 || d->zmax != 1.0) && dir==2) throw std::invalid_argument("Domain has incorrect values. Expected y E [0.0, 1.0]\n"); 
 
   for (int i(0); i<d->Nx; i++) {
     for (int j(0); j<d->Ny; j++) {
