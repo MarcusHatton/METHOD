984b3ed86117bae214b720d3a55d3ead3c473934
Marcus J Hatton <mjh1n20@soton.ac.uk>
2021-09-21 02:00:35 +0100

    sw to alt C2P

--- Examples/IS/Shocktube1D/main.cc
+++ Examples/IS/Shocktube1D/main.cc
@@ -65,7 +65,7 @@ int main(int argc, char *argv[]) {
   Data data(data_args, &env);
 
   // Choose particulars of simulation
-  IS model(&data, false);
+  IS model(&data, true); // bool for alt_C2P
 
   Weno3 weno(&data);
 
