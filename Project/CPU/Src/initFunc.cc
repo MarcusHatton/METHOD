c8ab1d824c84b326708e2f2239fcb6118dd6f356
Marcus J Hatton <mjh1n20@soton.ac.uk>
2021-09-24 18:28:40 +0100

    fix

--- Project/CPU/Src/initFunc.cc
+++ Project/CPU/Src/initFunc.cc
@@ -1293,7 +1293,7 @@ IS_Shocktube_1D_Perp::IS_Shocktube_1D_Perp(Data * data, int dir) : InitialFunc(d
 
 }
 
-IS_Shocktube_1D_Para::ISCE_Shocktube_1D_Para(Data * data, int dir) : InitialFunc(data)
+IS_Shocktube_1D_Para::IS_Shocktube_1D_Para(Data * data, int dir) : InitialFunc(data)
 {
   // Syntax
   Data * d(data);
