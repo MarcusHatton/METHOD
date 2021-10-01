ff5e4f7e19d06756b7623f7eefe91d99c1f015b5
MarcusHatton <74266732+MarcusHatton@users.noreply.github.com>
2021-10-01 02:35:22 +0100

    erf for vy in sheartest

--- Project/CPU/Src/initFunc.cc
+++ Project/CPU/Src/initFunc.cc
@@ -1527,19 +1527,11 @@ IS_ShearTest::IS_ShearTest(Data * data) : InitialFunc(data)
     for (int j(0); j<d->Ny; j++) {
       for (int k(0); k<d->Nz; k++) {
         
-        if (d->x[i] < 0.5 ) {
-          d->prims[ID(3, i, j, k)] = 1.0;
-          d->prims[ID(5, i, j, k)] = 1.0;
-          d->prims[ID(1, i, j, k)] = 0.1;
-          d->prims[ID(0, i, j, k)] = 0;
-          d->prims[ID(2, i, j, k)] = 0;
-        } else {
-          d->prims[ID(3, i, j, k)] = 1.0;
-          d->prims[ID(5, i, j, k)] = 1.0;
-          d->prims[ID(1, i, j, k)] = -0.1;
-          d->prims[ID(0, i, j, k)] = 0;  
-          d->prims[ID(2, i, j, k)] = 0;  
-        }
+        d->prims[ID(3, i, j, k)] = 1.0;
+        d->prims[ID(5, i, j, k)] = 1.0;
+        d->prims[ID(1, i, j, k)] = erf(6*(d->x[i] - 0.5));
+        d->prims[ID(0, i, j, k)] = 0;
+        d->prims[ID(2, i, j, k)] = 0;
 
       }
     }
