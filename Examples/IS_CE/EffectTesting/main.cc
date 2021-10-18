778e3c97e30224427d07ea225f1c2b84e57c60d1
MarcusHatton <74266732+MarcusHatton@users.noreply.github.com>
2021-10-18 22:12:04 +0100

    scaling test setup for ISCE too now

--- Examples/IS_CE/EffectTesting/main.cc
+++ Examples/IS_CE/EffectTesting/main.cc
@@ -19,11 +19,18 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
+  int nxs[] = {100,200,400,800,1600,3200};
+  int nx = 0;
+
+  for(int i=0; i<6; i++) {
+    nx = nxs[i];
+    cout << nx << std::endl;
+    std::string dirpath = "./1d/shear/"+std::to_string(nx);
+    mkdir(dirpath.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    
   // Set up domain
   int Ng(4);
-  // int nx(65536);
-  // int nx(32768);
-  int nx(400);
+  //int nx(400);
   int ny(0);
   int nz(0);
   double xmin(-1.0);
@@ -32,7 +39,7 @@ int main(int argc, char *argv[]) {
   double ymax(1.0);
   double zmin(0.0);
   double zmax(1.0);
-  double endTime(0.8);
+  double endTime(6.0);
   double cfl(0.1);
   // double gamma(0.001);
   // double sigma(0.001);
@@ -57,7 +64,7 @@ int main(int argc, char *argv[]) {
   data_args.sCfl(cfl);
   data_args.sNg(Ng);
   data_args.gamma = 5.0/3.0;
-  const std::vector<double> toy_params           { {1.0e-15, 1.0e-1,  1.0e-2, 1.0e-1,  1.0e-15, 1.0e-1} };
+  const std::vector<double> toy_params           { {1.0e-15, 1.0e-1,  1.0e-15, 1.0e-1,  1.0e-2, 1.0e-1} };
   const std::vector<std::string> toy_param_names = {"kappa", "tau_q", "zeta", "tau_Pi", "eta", "tau_pi"};
   const int n_toy_params(6);
   data_args.sOptionalSimArgs(toy_params, toy_param_names, n_toy_params);
@@ -84,15 +91,15 @@ int main(int argc, char *argv[]) {
   // Blob2dToyQ init(&data);
   //ISKHInstabilitySingleFluid init(&data, 1);
   //Shocktube_Chab21 init(&data);  
-  //IS_ShearTest init(&data);
-  IS_BulkHeatTest init(&data);
+  IS_ShearTest init(&data);
+  //IS_BulkHeatTest init(&data);
 
   // RKSplit timeInt(&data, &model, &bcs, &fluxMethod);
   // BackwardsRK2 timeInt(&data, &model, &bcs, &fluxMethod);
   // SSP2 timeInt(&data, &model, &bcs, &fluxMethod);
   RK2B timeInt(&data, &model, &bcs, &fluxMethod, &ModelExtension);
 
-  SerialSaveDataHDF5 save(&data, &env, "1d/bulk/data_serial_TIx_0", SerialSaveDataHDF5::OUTPUT_ALL);
+  SerialSaveDataHDF5 save(&data, &env, "1d/shear/"+std::to_string(nx)+"/data_serial_TIx_0", SerialSaveDataHDF5::OUTPUT_ALL);
 
   // Now objects have been created, set up the simulation
   sim.set(&init, &model, &timeInt, &bcs, &fluxMethod, &save);
@@ -101,10 +108,12 @@ int main(int argc, char *argv[]) {
 
   for (int n(0); n<nreports; n++) {
     data.endTime = (n+1)*endTime/(nreports);
-    SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/bulk/data_serial_TIx_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
+    SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/shear/"+std::to_string(nx)+"data_serial_TIx_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
     sim.evolve(output);
     save_in_loop.saveAll();
   }
+
+  } // end of for-loop
 
   return 0;
 
