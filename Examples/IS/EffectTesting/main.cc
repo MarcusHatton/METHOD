02ffb004906617a3053fe2cb94fa13159d5295ac
Marcus J Hatton <mjh1n20@soton.ac.uk>
2021-11-04 00:19:02 +0000

    main changes

--- Examples/IS/EffectTesting/main.cc
+++ Examples/IS/EffectTesting/main.cc
@@ -19,29 +19,28 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
+  int nxs[] = {1000,2000,4000,8000,16000};
+  int nx = 0;
 
-  float etas[] = {5e-2, 1e-2, 5e-2, 1e-3, 1e-4, 1e-5};
-  float eta = 0;
-
-  for(int i=0; i<6; i++) {
-    eta = etas[i];
-    cout << eta << std::endl;
-    std::string dirpath = "./1d/shear/etas/"+std::to_string(eta);
+  for(int i=0; i<5; i++) {
+    nx = nxs[i];
+    //cout << nx << std::endl;
+    std::string dirpath = "./1d/shear/res/"+std::to_string(nx);
     mkdir(dirpath.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
   
   // Set up domain
   int Ng(4);
-  int nx(4000);
+  //int nx(400);
   //if(argc>=2) { nx=atoi(argv[1]); }
   int ny(0);
   int nz(0);
-  double xmin(-10.0);
-  double xmax(10.0);
+  double xmin(-1.0);
+  double xmax(1.0);
   double ymin(0.0);
   double ymax(1.0);
   double zmin(0.0);
   double zmax(1.0);
-  double endTime(6.0);
+  double endTime(50.0);
   double cfl(0.1);
   // double gamma(0.001);
   // double sigma(0.001);
@@ -66,7 +65,7 @@ int main(int argc, char *argv[]) {
   data_args.sCfl(cfl);
   data_args.sNg(Ng);
   data_args.gamma = 5.0/3.0;
-  const std::vector<double> toy_params           { {1.0e-15, 1.0e-1,  1.0e-15, 1.0e-1,  eta, 1e-1} };
+  const std::vector<double> toy_params           { {1.0e-15, 1.0e-1,  1.0e-15, 1.0e-1,  1.0e-3, 1.0e-1} };
   const std::vector<std::string> toy_param_names = {"kappa", "tau_q", "zeta", "tau_Pi", "eta", "tau_pi"};
   const int n_toy_params(6);
   data_args.sOptionalSimArgs(toy_params, toy_param_names, n_toy_params);
@@ -97,7 +96,7 @@ int main(int argc, char *argv[]) {
   // SSP2 timeInt(&data, &model, &bcs, &fluxMethod);
   RK2B timeInt(&data, &model, &bcs, &fluxMethod);
 
-  SerialSaveDataHDF5 save(&data, &env, "1d/shear/etas/"+std::to_string(eta)+"/data_serial_TIx_0", SerialSaveDataHDF5::OUTPUT_ALL);
+  SerialSaveDataHDF5 save(&data, &env, "1d/shear/res/"+std::to_string(nx)+"/data_serial_TIx_0", SerialSaveDataHDF5::OUTPUT_ALL);
 
   // Now objects have been created, set up the simulation
   sim.set(&init, &model, &timeInt, &bcs, &fluxMethod, &save);
@@ -106,7 +105,7 @@ int main(int argc, char *argv[]) {
 
   for (int n(0); n<nreports; n++) {
     data.endTime = (n+1)*endTime/(nreports);
-    SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/shear/etas/"+std::to_string(eta)+"/data_serial_TIx_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
+    SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/shear/res/"+std::to_string(nx)+"/data_serial_TIx_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
     sim.evolve(output);
     save_in_loop.saveAll();
   }
