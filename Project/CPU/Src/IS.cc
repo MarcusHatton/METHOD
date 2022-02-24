110583db9f2eea414d563868a57a72da0b26a253
MarcusHatton <74266732+MarcusHatton@users.noreply.github.com>
2022-02-24 18:12:57 +0000

    debugging

--- Project/CPU/Src/IS.cc
+++ Project/CPU/Src/IS.cc
@@ -1055,7 +1055,7 @@ void IS::fluxVector(double *cons, double *prims, double *aux, double *f, const i
           + aux[ID(Aux::pi01+dir, i, j, k)];
       
         if (i==4 && j==0 && k==0)
-          std::cout << f[ID(2, i, j, k)] << std::endl;
+          std::cout << f[ID(1, i, j, k)] << std::endl;
       
       } // End k loop
     } // End j loop
