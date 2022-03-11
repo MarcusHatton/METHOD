91c0df7b6c929b9d8f80894d5479994673e5d781
Marcus Hatton <mjh1n20@Ians-MacBook-Air-4.local>
2022-03-11 14:29:15 +0000

    modifying c2P test

--- Project/CPU/Include/IS.h
+++ Project/CPU/Include/IS.h
@@ -26,7 +26,7 @@ class IS : public Model
       @parm
       @param *data Pointer to Data class containing global simulation data
     */
-    IS(Data * data, bool alternative_C2P);
+    IS(Data * data);
 
     virtual ~IS();     //!< Destructor
 
