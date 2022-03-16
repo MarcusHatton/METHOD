a30442c2ce1c105624f208b9772f1becb9009585
Marcus J Hatton <mjh1n20@soton.ac.uk>
2022-03-16 19:31:18 +0000

    silly

--- Examples/BDNK/C2P_Test/main.cc
+++ Examples/BDNK/C2P_Test/main.cc
@@ -202,7 +202,10 @@ int main(int argc, char *argv[]) {
   // Triple...
   model.primsToAll(d->cons, d->prims, d->aux);
   model.getPrimitiveVars(d->cons, d->prims, d->aux);
-
+  model.primsToAll(d->cons, d->prims, d->aux);
+  model.getPrimitiveVars(d->cons, d->prims, d->aux);  model.primsToAll(d->cons, d->prims, d->aux);
+  model.getPrimitiveVars(d->cons, d->prims, d->aux);  model.primsToAll(d->cons, d->prims, d->aux);
+  model.getPrimitiveVars(d->cons, d->prims, d->aux);
 
   //Cross-check
   for (int i(d->is); i < d->ie; i++) {
