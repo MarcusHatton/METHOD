a6a58d5a56958896048da9d6517f6b49aa343eb4
Marcus J Hatton <mjh1n20@soton.ac.uk>
2022-03-23 18:45:33 +0000

    main.cc

--- Examples/BDNK/Shocktube1D/main_testing.cc
+++ Examples/BDNK/Shocktube1D/main_testing.cc
@@ -89,7 +89,7 @@ int main(int argc, char *argv[]) {
   // SSP2 timeInt(&data, &model, &bcs, &fluxMethod);
   RK2B timeInt(&data, &model, &bcs, &fluxMethod);
 
-  SerialSaveDataHDF5 save(&data, &env, "1d/all/data_serial_TIx_0", SerialSaveDataHDF5::OUTPUT_ALL);
+  SerialSaveDataHDF5 save(&data, &env, "1d/testing/data_serial_TIx_0", SerialSaveDataHDF5::OUTPUT_ALL);
 
   // Now objects have been created, set up the simulation
   sim.set(&init, &model, &timeInt, &bcs, &fluxMethod, &save);
@@ -99,7 +99,7 @@ int main(int argc, char *argv[]) {
   for (int n(0); n<nreports; n++) {
     data.endTime = (n+1)*endTime/(nreports);
     //data.endTime = 4.0e-3; // HACK
-    SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/all/data_serial_TIx_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
+    SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/testing/data_serial_TIx_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
     sim.evolve(output);
     save_in_loop.saveAll();
   }
