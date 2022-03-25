b30cc8b5ed12040b57dbde11887051076bed8f07
Marcus J Hatton <mjh1n20@soton.ac.uk>
2022-03-25 16:37:30 +0000

    werk

--- Project/CPU/Src/IS.cc
+++ Project/CPU/Src/IS.cc
@@ -969,9 +969,10 @@ void IS::fluxVector(double *cons, double *prims, double *aux, double *f, const i
           throw std::runtime_error("Flux direction is not 0, 1 or 2");
         }
 
-        if (i==401 && j==0 && k==0)
+        if (i==401 && j==0 && k==0) {
           std::cout << cons[ID(Cons::S1, i, j, k)] << "\t" << f[ID(1, i, j, k)] << "\t" << aux[ID(Aux::q1, i, j, k)] std::endl;
-
+        }
+        
         // (Tau+p)*v + ...
         f[ID(4, i, j, k)] = (cons[ID(Cons::Tau, i, j, k)] + prims[ID(Prims::p, i, j, k)]) * prims[ID(dir, i, j, k)] 
           + (aux[ID(Aux::q1+dir, i, j, k)] - aux[ID(Aux::qv, i, j, k)]*prims[ID(Prims::v1+dir, i, j, k)])*aux[ID(Aux::W, i, j, k)]
