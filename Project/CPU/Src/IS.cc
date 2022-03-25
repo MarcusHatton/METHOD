0f60049feb73e22603c81ae68b1d3367b1158ff8
Marcus J Hatton <mjh1n20@soton.ac.uk>
2022-03-25 16:36:46 +0000

    werk

--- Project/CPU/Src/IS.cc
+++ Project/CPU/Src/IS.cc
@@ -970,7 +970,7 @@ void IS::fluxVector(double *cons, double *prims, double *aux, double *f, const i
         }
 
         if (i==401 && j==0 && k==0)
-          std::cout << f[ID(1, i, j, k)] << std::endl;
+          std::cout << cons[ID(Cons::S1, i, j, k)] << "\t" << f[ID(1, i, j, k)] << "\t" << aux[ID(Aux::q1, i, j, k)] std::endl;
 
         // (Tau+p)*v + ...
         f[ID(4, i, j, k)] = (cons[ID(Cons::Tau, i, j, k)] + prims[ID(Prims::p, i, j, k)]) * prims[ID(dir, i, j, k)] 
