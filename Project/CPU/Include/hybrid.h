d4bd057ffe1bde148f15867c410dc032cbce61e7
MarcusHatton <74266732+MarcusHatton@users.noreply.github.com>
2022-06-09 18:21:36 +0100

    hybrid werk

--- Project/CPU/Include/hybrid.h
+++ Project/CPU/Include/hybrid.h
@@ -52,7 +52,7 @@ class Hybrid : public Model
     *iflux, *dflux,                             // Flux vectors for ideal, resistive and DEIFY. Size is \f$N_{cons}*N_x*N_y*N_z\f$.
     *isource, *dsource, *deifySource,          // Source vectors for ideal, resistive and DEIFY. Size is \f$N_{cons}*N_x*N_y*N_z\f$.
     tauCrossOver,                             // Centre conductivity of penalty function
-    tauSpan,                                  // Span of conductivity of penalty function
+    tauSpan;                                  // Span of conductivity of penalty function
 
 
     bool useDEIFY;                             // Should we use DEIFY? (Default to true)
