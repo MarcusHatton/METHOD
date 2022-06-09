01ae966cd526788dc08a23b0d6b8ee3d24ccd5f5
MarcusHatton <74266732+MarcusHatton@users.noreply.github.com>
2022-06-09 01:00:16 +0100

    hybrid tings

--- Project/CPU/Include/hybrid.h
+++ Project/CPU/Include/hybrid.h
@@ -302,7 +302,7 @@ class Hybrid : public Model
     // void finalise(double *cons, double *prims, double *aux);
     void finalise(double *cons, double *prims, double *aux, bool final_step=false) { 
      
-        dissipativeModel->finalise(double *cons, double *prims, double *aux, bool final_step=false);
+        dissipativeModel->finalise(cons, prims, aux, final_step);
 
     };      
 
