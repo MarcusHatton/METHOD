ac8637f92a453ed6b8950cb42c864d40e8b8b1a1
Marcus J Hatton <mjh1n20@soton.ac.uk>
2022-06-10 14:26:28 +0100

    hybrid werk

--- Project/CPU/Src/hybrid.cc
+++ Project/CPU/Src/hybrid.cc
@@ -157,7 +157,7 @@ double Hybrid::idealWeightID(double * cons, double * prims, double * aux, int i,
 bool Hybrid::useDissipative(double * cons, double * prims, double * aux)
 {
   // Should we use the Dissipative (IS) C2P?
-  if (data->tauFunc(cons, prims, aux) > tauCrossOver) {
+  if (data->tauFunc(cons, prims, aux) > tauCrossOver - tauSpan) {
       printf("IS");
     // std::cout << "Yes";
   } else { 
