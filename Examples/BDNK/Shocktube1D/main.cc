5b0cd409d5cc0887d661b9db87e2dc9d3d212bc1
Marcus J Hatton <mjh1n20@soton.ac.uk>
2022-06-28 15:17:58 +0100

    changing name of BDNK files from IS.cc/h... still not changing actual class names though I should...

--- Examples/BDNK/Shocktube1D/main.cc
+++ Examples/BDNK/Shocktube1D/main.cc
@@ -2,7 +2,7 @@
 #include "simData.h"
 #include "simulation.h"
 #include "initFunc.h"
-#include "IS.h"
+#include "BDNK.h"
 #include "boundaryConds.h"
 // #include "rkSplit.h"
 // #include "backwardsRK.h"
