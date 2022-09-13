57712dd9b235f01dc83ddb11d1aeb699a0cb23a1
Marcus J Hatton <mjh1n20@soton.ac.uk>
2022-09-13 18:07:21 +0100

    GPU stuff and mains/Makefiles

--- Examples/IS_CE/KH2D_speedtesting/main.cc
+++ Examples/IS_CE/KH2D_speedtesting/main.cc
@@ -27,8 +27,8 @@ int main(int argc, char *argv[]) {
   int Ng(4);
   // int nx(65536);
   // int nx(32768);
-  int nx(200);
-  int ny(200);
+  int nx(400);
+  int ny(400);
   int nz(0);
   double xmin(-0.5);
   double xmax(0.5);
@@ -63,7 +63,7 @@ int main(int argc, char *argv[]) {
   data_args.sNg(Ng);
   data_args.gamma = 4.0/3.0;
   data_args.reportItersPeriod = 2000;
-  const std::vector<double> toy_params           { {1.0e-15, 1.0e-4,  1.0e-15, 1.0e-4,  5.0e-4, 1.0e-4} };
+  const std::vector<double> toy_params           { {1.0e-15, 1.0e-4,  1.0e-15, 1.0e-4,  1.0e-2, 1.0e-4} };
   const std::vector<std::string> toy_param_names = {"kappa", "tau_q", "zeta", "tau_Pi", "eta", "tau_pi"};
   const int n_toy_params(6);
   data_args.sOptionalSimArgs(toy_params, toy_param_names, n_toy_params);
--- Examples/IS_CE/Shocktube1D/main.cc
+++ Examples/IS_CE/Shocktube1D/main.cc
@@ -2,7 +2,7 @@
 #include "simData.h"
 #include "simulation.h"
 #include "initFunc.h"
-#include "ISCE.h"
+//#include "ISCE.h"
 #include "boundaryConds.h"
 // #include "rkSplit.h"
 // #include "backwardsRK.h"
