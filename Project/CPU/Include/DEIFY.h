fc83abcebb810181a965d0fce1dd32b0e39c5af6
Marcus J Hatton <mjh1n20@soton.ac.uk>
2022-10-12 20:39:10 +0100

    putting minmod grad calcs back in...

--- Project/CPU/Include/DEIFY.h
+++ Project/CPU/Include/DEIFY.h
@@ -157,27 +157,27 @@ class DEIFY : public ModelExtension
 };
 
 // First and second order "minmod" functions for slope-limiting
-// double minmodGradFO(double im1, double i, double ip1, double dX) {
+double minmodGradFO(double im1, double i, double ip1, double dX) {
 
-//   double FDGrad = (-1.0*i + 1*ip1)/dX;
-//   double BDGrad = (1.0*i - 1*im1)/dX;
-//   if ( (FDGrad < 0 && BDGrad > 0) || (FDGrad > 0 && BDGrad < 0) ) {
-//     return 0;
-//   } else {
-//     return abs(FDGrad) < abs(BDGrad) ? FDGrad : BDGrad;
-//   }
-// }
+  double FDGrad = (-1.0*i + 1*ip1)/dX;
+  double BDGrad = (1.0*i - 1*im1)/dX;
+  if ( (FDGrad < 0 && BDGrad > 0) || (FDGrad > 0 && BDGrad < 0) ) {
+    return 0;
+  } else {
+    return abs(FDGrad) < abs(BDGrad) ? FDGrad : BDGrad;
+  }
+}
 
-// double minmodGradSO(double im2, double im1, double i, double ip1, double ip2, double dX) {
+double minmodGradSO(double im2, double im1, double i, double ip1, double ip2, double dX) {
   
-//   double FDGrad = (-1.5*i + 2*ip1 - 0.5*ip2)/dX;
-//   double BDGrad = (1.5*i - 2*im1 + 0.5*im2)/dX;
-//   if ( (FDGrad < 0 && BDGrad > 0) || (FDGrad > 0 && BDGrad < 0) ) {
-//     return 0;
-//   } else {
-//     return abs(FDGrad) < abs(BDGrad) ? FDGrad : BDGrad;
-//   }
-// }
+  double FDGrad = (-1.5*i + 2*ip1 - 0.5*ip2)/dX;
+  double BDGrad = (1.5*i - 2*im1 + 0.5*im2)/dX;
+  if ( (FDGrad < 0 && BDGrad > 0) || (FDGrad > 0 && BDGrad < 0) ) {
+    return 0;
+  } else {
+    return abs(FDGrad) < abs(BDGrad) ? FDGrad : BDGrad;
+  }
+}
 
 // double minmidGradSOGeneral(int enum_number, int dir, int c_p_or_a, double * cons, double * prims, double * aux, int i, int j, int k) {
 //   double dx = d->dx;
