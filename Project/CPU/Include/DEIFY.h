923585163af58026f559cddf97d2732052244f4b
Marcus J Hatton <mjh1n20@soton.ac.uk>
2022-10-13 18:58:24 +0100

    putting minmod grad calcs back in...

--- Project/CPU/Include/DEIFY.h
+++ Project/CPU/Include/DEIFY.h
@@ -179,12 +179,11 @@ class DEIFY : public ModelExtension
 //   }
 // }
 
-double minmidGradSOGeneral(int enum_number, int dir, int c_p_or_a, double * cons, double * prims, double * aux, int i, int j, int k) {
+double minmidGradSOGeneral(int enum_number, int dir, int c_p_or_a, double * cons, double * prims, double * aux, int i, int j, int k, Data * d) {
   double FDSum = 0.0;
   double BDSum = 0.0;
   float Coeff_stencil [3] = {-1.5, 2.0, -0.5};
   int stencil [3] = {0,0,0};
-  Data * d(this->data);
   switch (dir) {
     case 0:
       dX = d->dx;
