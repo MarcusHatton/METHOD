5221753a85783d4ac5dc3b4beabbbb88940be43d
Marcus J Hatton <mjh1n20@soton.ac.uk>
2022-12-09 17:24:30 +0000

    just output directory stuff

--- Examples/IS_CE/KH2D_speedtesting/main.cc
+++ Examples/IS_CE/KH2D_speedtesting/main.cc
@@ -28,8 +28,8 @@ int main(int argc, char *argv[]) {
   int Ng(4);
   // int nx(65536);
   // int nx(32768);
-  int nx(400);
-  int ny(800);
+  int nx(20);
+  int ny(40);
   int nz(0);
   double xmin(-0.5);
   double xmax(0.5);
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]) {
   double zmin(-0.1);
   double zmax(0.1);
   double startTime(10.0); // When to start outputting HDF5 data
-  double endTime(15.0);
+  double endTime(0.1);
   double cfl(0.4);
   // double gamma(0.001);
   // double sigma(0.001);
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
   bool output(false);
   int nreports(51);
 
-  ParallelEnv env(&argc, &argv, 8, 5, 1);
+  ParallelEnv env(&argc, &argv, 2, 2, 1);
   //SerialEnv env(&argc, &argv, 1, 1, 1);
 
   DataArgs data_args(nx, ny, nz, xmin, xmax, ymin, ymax, zmin, zmax, endTime);
@@ -96,7 +96,7 @@ int main(int argc, char *argv[]) {
   RK2 timeInt(&data, &model, &bcs, &fluxMethod, &ModelExtension);
   // RKPlus timeInt(&data, &model, &bcs, &fluxMethod);
 
-  ParallelSaveDataHDF5 save(&data, &env, "2d/Shear/dp_"+std::to_string(nx)+"x"+std::to_string(ny)+"x"+std::to_string(nz)+"_0", ParallelSaveDataHDF5::OUTPUT_ALL);
+  ParallelSaveDataHDF5 save(&data, &env, "2d/tmp/dp_"+std::to_string(nx)+"x"+std::to_string(ny)+"x"+std::to_string(nz)+"_0", ParallelSaveDataHDF5::OUTPUT_ALL);
 
   // Now objects have been created, set up the simulation
   sim.set(&init, &model, &timeInt, &bcs, &fluxMethod, nullptr);
@@ -105,8 +105,8 @@ int main(int argc, char *argv[]) {
 
   for (int n(0); n<nreports; n++) {
     data.endTime = startTime + n*((endTime-startTime)/(nreports-1));
-    //ParallelSaveDataHDF5 save_in_loop(&data, &env, "2d/Shear/dp_"+std::to_string(nx)+"x"+std::to_string(ny)+"x"+std::to_string(nz)+"_"+std::to_string(n+1), ParallelSaveDataHDF5::OUTPUT_ALL);
-    ParallelSaveDataHDF5 save_in_loop(&data, &env, "./../../../../../../scratch/mjh1n20/Filtering_Data/KH/dp_"+std::to_string(nx)+"x"+std::to_string(ny)+"x"+std::to_string(nz)+"_"+std::to_string(n+1), ParallelSaveDataHDF5::OUTPUT_ALL);
+    ParallelSaveDataHDF5 save_in_loop(&data, &env, "2d/tmp/dp_"+std::to_string(nx)+"x"+std::to_string(ny)+"x"+std::to_string(nz)+"_"+std::to_string(n+1), ParallelSaveDataHDF5::OUTPUT_ALL);
+    //ParallelSaveDataHDF5 save_in_loop(&data, &env, "./../../../../../../scratch/mjh1n20/Filtering_Data/KH/dp_"+std::to_string(nx)+"x"+std::to_string(ny)+"x"+std::to_string(nz)+"_"+std::to_string(n+1), ParallelSaveDataHDF5::OUTPUT_ALL);
     sim.evolve(output);
     save_in_loop.saveAll();
   }
