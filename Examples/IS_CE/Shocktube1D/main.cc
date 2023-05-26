2c6e843c432408f4c83005cac42b3cb0b382f4f2
Marcus J Hatton <mjh1n20@soton.ac.uk>
2023-05-26 12:41:11 +0100

    resolution scaling testing

--- Examples/IS_CE/Shocktube1D/main.cc
+++ Examples/IS_CE/Shocktube1D/main.cc
@@ -2,7 +2,7 @@
 #include "simData.h"
 #include "simulation.h"
 #include "initFunc.h"
-//#include "ISCE.h"
+#include "ISCE.h"
 #include "boundaryConds.h"
 // #include "rkSplit.h"
 // #include "backwardsRK.h"
@@ -23,11 +23,11 @@ int main(int argc, char *argv[]) {
   int Ng(4);
   // int nx(65536);
   // int nx(32768);
-  int nx(800);
+  int nx(600);
   int ny(0);
   int nz(0);
-  double xmin(0.0);
-  double xmax(5.0);
+  double xmin(-2.0);
+  double xmax(3.0);
   double ymin(0.0);
   double ymax(1.0);
   double zmin(0.0);
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
   data_args.sCfl(cfl);
   data_args.sNg(Ng);
   data_args.gamma = 5.0/3.0;
-  const std::vector<double> toy_params           { {1.0e-15, 1.0e-1,  1.0e-3, 1.0e-1,  1.0e-15, 1.0e-1} };
+  const std::vector<double> toy_params           { {1.0e-15, 1.0e-1,  5.0e-2, 1.0e-2,  1.0e-15, 1.0e-1} };
   const std::vector<std::string> toy_param_names = {"kappa", "tau_q", "zeta", "tau_Pi", "eta", "tau_pi"};
   const int n_toy_params(6);
   data_args.sOptionalSimArgs(toy_params, toy_param_names, n_toy_params);
@@ -69,7 +69,7 @@ int main(int argc, char *argv[]) {
 
   Weno3 weno(&data);
 
-  FVS fluxMethod(&data, &model);
+  FVS fluxMethod(&data, &weno, &model);
 
   DEIFY ModelExtension(&data, &fluxMethod);
 
@@ -92,7 +92,7 @@ int main(int argc, char *argv[]) {
   // SSP2 timeInt(&data, &model, &bcs, &fluxMethod);
   RK2B timeInt(&data, &model, &bcs, &fluxMethod, &ModelExtension);
 
-  SerialSaveDataHDF5 save(&data, &env, "1d/bulk/data_serial_TIx_0", SerialSaveDataHDF5::OUTPUT_ALL);
+  SerialSaveDataHDF5 save(&data, &env, "1d/Bulk/ds_"+std::to_string(nx)+"_0", SerialSaveDataHDF5::OUTPUT_ALL);
 
   // Now objects have been created, set up the simulation
   sim.set(&init, &model, &timeInt, &bcs, &fluxMethod, &save);
@@ -101,7 +101,7 @@ int main(int argc, char *argv[]) {
 
   for (int n(0); n<nreports; n++) {
     data.endTime = (n+1)*endTime/(nreports);
-    SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/bulk/data_serial_TIx_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
+    SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/Bulk/ds_"+std::to_string(nx)+"_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
     sim.evolve(output);
     save_in_loop.saveAll();
   }
