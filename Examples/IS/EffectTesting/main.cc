2490f4a99c7580bf30707032e47a39753a6df1fd
Marcus J Hatton <mjh1n20@soton.ac.uk>
2023-06-28 17:32:47 +0100

    mains and submitMes

--- Examples/IS/EffectTesting/main.cc
+++ Examples/IS/EffectTesting/main.cc
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
   for(int i=0; i<4; i++) {
     tau_pi = tau_pis[i];
     cout << tau_pi << std::endl;
-    std::string dirpath = "./1d/StillShock/Bulk/taus/"+std::to_string(tau_pi);
+    std::string dirpath = "./1d/StillShock/Bulk/tau_scaling/"+std::to_string(tau_pi);
     mkdir(dirpath.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
   
   // Set up domain
@@ -66,7 +66,7 @@ int main(int argc, char *argv[]) {
   data_args.sCfl(cfl);
   data_args.sNg(Ng);
   data_args.gamma = 5.0/3.0;
-  const std::vector<double> toy_params           { {1.0e-15, 1.0e-1,  1.0e-3, tau_pi,  1.0e-15, 1.0e-1} };
+  const std::vector<double> toy_params           { {1.0e-15, 1.0e-1,  1.0e-2, tau_pi,  1.0e-15, 1.0e-1} };
   const std::vector<std::string> toy_param_names = {"kappa", "tau_q", "zeta", "tau_Pi", "eta", "tau_pi"};
   const int n_toy_params(6);
   data_args.sOptionalSimArgs(toy_params, toy_param_names, n_toy_params);
@@ -97,7 +97,7 @@ int main(int argc, char *argv[]) {
   SSP2 timeInt(&data, &model, &bcs, &fluxMethod);
   // RK2B timeInt(&data, &model, &bcs, &fluxMethod);
 
-  SerialSaveDataHDF5 save(&data, &env, "1d/StillShock/Bulk/taus/"+std::to_string(tau_pi)+"/ds_0", SerialSaveDataHDF5::OUTPUT_ALL);
+  SerialSaveDataHDF5 save(&data, &env, "1d/StillShock/Bulk/tau_scaling/"+std::to_string(tau_pi)+"/ds_0", SerialSaveDataHDF5::OUTPUT_ALL);
 
   // Now objects have been created, set up the simulation
   sim.set(&init, &model, &timeInt, &bcs, &fluxMethod, &save);
@@ -106,7 +106,7 @@ int main(int argc, char *argv[]) {
 
   for (int n(0); n<nreports; n++) {
     data.endTime = (n+1)*endTime/(nreports);
-    SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/StillShock/Bulk/taus/"+std::to_string(tau_pi)+"/ds_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
+    SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/StillShock/Bulk/tau_scaling/"+std::to_string(tau_pi)+"/ds_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
     sim.evolve(output);
     save_in_loop.saveAll();
   }
