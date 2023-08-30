0b48559d75d2f306c1ece04d03219304911027b7
MarcusHatton <74266732+MarcusHatton@users.noreply.github.com>
2023-08-30 17:03:14 +0100

    fixes

--- Project/CPU/Include/DEIFY.h
+++ Project/CPU/Include/DEIFY.h
@@ -76,28 +76,31 @@ class DEIFY : public ModelExtension
     template<typename T>
     T sqr(T x) { return ((x) * (x)); }
 
-    // First and second order "minmod" functions for slope-limiting
-    double minmodGradFO(double im1, double i, double ip1, double dX) {
+    template<typename T>
+    T cube(T x) { return ((x) * (x) * (x)); }
 
-      double FDGrad = (-1.0*i + 1*ip1)/dX;
-      double BDGrad = (1.0*i - 1*im1)/dX;
-      if ( (FDGrad < 0 && BDGrad > 0) || (FDGrad > 0 && BDGrad < 0) ) {
-        return 0;
-      } else {
-        return abs(FDGrad) < abs(BDGrad) ? FDGrad : BDGrad;
-      }
-    }
+    // // First and second order "minmod" functions for slope-limiting
+    // double minmodGradFO(double im1, double i, double ip1, double dX) {
 
-    double minmodGradSO(double im2, double im1, double i, double ip1, double ip2, double dX) {
+    //   double FDGrad = (-1.0*i + 1*ip1)/dX;
+    //   double BDGrad = (1.0*i - 1*im1)/dX;
+    //   if ( (FDGrad < 0 && BDGrad > 0) || (FDGrad > 0 && BDGrad < 0) ) {
+    //     return 0;
+    //   } else {
+    //     return abs(FDGrad) < abs(BDGrad) ? FDGrad : BDGrad;
+    //   }
+    // }
+
+    // double minmodGradSO(double im2, double im1, double i, double ip1, double ip2, double dX) {
       
-      double FDGrad = (-1.5*i + 2*ip1 - 0.5*ip2)/dX;
-      double BDGrad = (1.5*i - 2*im1 + 0.5*im2)/dX;
-      if ( (FDGrad < 0 && BDGrad > 0) || (FDGrad > 0 && BDGrad < 0) ) {
-        return 0;
-      } else {
-        return abs(FDGrad) < abs(BDGrad) ? FDGrad : BDGrad;
-      }
-    }
+    //   double FDGrad = (-1.5*i + 2*ip1 - 0.5*ip2)/dX;
+    //   double BDGrad = (1.5*i - 2*im1 + 0.5*im2)/dX;
+    //   if ( (FDGrad < 0 && BDGrad > 0) || (FDGrad > 0 && BDGrad < 0) ) {
+    //     return 0;
+    //   } else {
+    //     return abs(FDGrad) < abs(BDGrad) ? FDGrad : BDGrad;
+    //   }
+    // }
     
     // enums to save looking up numbering of C/P/As when using ID accessor.
     enum Cons { D, S1, S2, S3, Tau };
