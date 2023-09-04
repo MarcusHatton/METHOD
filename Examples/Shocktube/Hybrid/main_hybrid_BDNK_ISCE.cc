64e5938680aa4328e2d1e8dd992caa93f6dc4fb3
Marcus J Hatton <mjh1n20@soton.ac.uk>
2023-09-04 16:44:53 +0100

    new structure with ST&KHI folders within examples, with model folders within those

---
+++ Examples/Shocktube/Hybrid/main_hybrid_BDNK_ISCE.cc
@@ -0,0 +1,122 @@
+/**
+ * @Author: Alex James Wright <alex>
+ * @Date:   2019-09-30T15:33:00+01:00
+ * @Email:  alex.j.wright2@gmail.com
+ * @Last modified by:   alex
+ * @Last modified time: 2020-02-05T15:48:38+00:00
+ * @License: MIT
+ */
+
+// Serial main
+#include "simData.h"
+#include "simulation.h"
+#include "initFunc.h"
+#include "boundaryConds.h"
+#include "rkSplit2ndOrder.h"
+#include "fluxVectorSplitting.h"
+#include "hybrid_BDNK_ISCE.h"
+#include "serialSaveDataHDF5.h"
+#include "serialEnv.h"
+#include "weno.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+
+  // Set up domain
+  int Ng(4);
+  int nx(400);
+  int ny(0);
+  int nz(0);
+  double xmin(0.0);
+  double xmax(1.0);
+  double ymin(0.0);
+  double ymax(1.0);
+  double zmin(0.0);
+  double zmax(1.0);
+  double endTime(0.4);
+  double cfl(0.1);
+  // double cp(1.0);
+  // int frameSkip(40);
+  bool output(false);
+  int safety(-1);
+
+  // HYBRID PARAMETERS
+  double tau(1.0e-2); // Unused currently
+  double tauCrossOver(1.0);
+  double tauSpan(5.0e-1); 
+  bool functionalTau(true); // hard-coded or calc'd on-the-fly?
+  bool useDEIFY(true); // add source-extension when near-ideal limit?
+  int nreports(10);
+
+  SerialEnv env(&argc, &argv, 1, 1, 1);
+
+  DataArgs data_args(nx, ny, nz, xmin, xmax, ymin, ymax, zmin, zmax, endTime);
+  data_args.gamma = 5.0/3.0;
+  data_args.sCfl(cfl);
+  data_args.sNg(Ng);
+  data_args.sfunctionalTau(functionalTau);
+  data_args.sTau(tau);
+  double eta_0 = 1.0;
+  const std::vector<double> toy_params           { {1.0e-15, (25/7)*eta_0,  5.0e-2, eta_0, (25/4)*eta_0} };
+  const std::vector<std::string> toy_param_names = {"kappa", "lambda_0", "zeta", "eta_0", "chi_0"};
+  const int n_toy_params(5);
+  data_args.sOptionalSimArgs(toy_params, toy_param_names, n_toy_params);
+  Data data(data_args, &env);
+
+  // Data data(nx, ny, nz, xmin, xmax, ymin, ymax, zmin, zmax, endTime, &env,
+  //           cfl, Ng, xi, tau, cp, frameSkip);
+
+  // Choose particulars of simulation
+  // Hybrid model(&data, tauCrossOver, eta0CrossOver, useDEIFY);
+  Hybrid model(&data, tauCrossOver, tauSpan, useDEIFY);
+
+  Weno3 weno(&data);
+
+  FVS fluxMethod(&data, &weno, &model);
+
+  model.setupDEIFY(&fluxMethod);
+
+  Outflow bcs(&data);
+
+  Simulation sim(&data, &env);
+
+  IS_Shocktube_1D_Para init(&data, 0); //direction given by second arg (int)
+
+  RKSplit2 timeInt(&data, &model, &bcs, &fluxMethod, NULL);
+  // RK2B timeInt(&data, &model, &bcs, &fluxMethod);
+
+  // SerialSaveData save(&data, &env, 1);
+  SerialSaveDataHDF5 save(&data, &env, "1d/ideal/data_serial_0", SerialSaveDataHDF5::OUTPUT_ALL);
+
+  // Now objects have been created, set up the simulation
+  sim.set(&init, &model, &timeInt, &bcs, &fluxMethod, &save);
+
+  // Time execution of programme
+  clock_t startTime(clock());
+
+  // Run until end time and save results
+  // sim.evolve(output, safety);
+
+  double timeTaken(double(clock() - startTime)/(double)CLOCKS_PER_SEC);
+
+  save.saveAll();
+  printf("\nRuntime: %.5fs\nCompleted %d iterations.\n", timeTaken, data.iters);
+
+  for (int n(0); n<nreports; n++) {
+    data.endTime = (n+1)*endTime/(nreports);
+    SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/ideal/data_serial_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
+    sim.evolve(output);
+    save_in_loop.saveAll();
+  }
+
+  return 0;
+
+}
---
+++ Examples/Shocktube/IS/main.cc
@@ -0,0 +1,105 @@
+// Serial main
+#include "simData.h"
+#include "simulation.h"
+#include "initFunc.h"
+#include "IS.h"
+#include "boundaryConds.h"
+// #include "rkSplit.h"
+// #include "backwardsRK.h"
+#include "RKPlus.h"
+// #include "SSP2.h"
+#include "fluxVectorSplitting.h"
+#include "serialEnv.h"
+#include "serialSaveDataHDF5.h"
+#include "weno.h"
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+  // Set up domain
+  int Ng(4);
+  // int nx(65536);
+  // int nx(32768);
+  int nx(400);
+  int ny(0);
+  int nz(0);
+  double xmin(0.0);
+  double xmax(5.0);
+  double ymin(0.0);
+  double ymax(1.0);
+  double zmin(0.0);
+  double zmax(1.0);
+  double endTime(2.0);
+  double cfl(0.1);
+  // double gamma(0.001);
+  // double sigma(0.001);
+  // These parameters work with IMEX SSP2; given that tau_q << dt,
+  // we should not expect them to work with the explicit solver, and indeed
+  // it fails very quickly.
+  // Note it's the ratio that matters to it being stable with IMEX.
+  // Need gamma/sigma <~ 1 or so, o/w wavespeed will be too
+  // big, and things fail. At moderate resolution you can get away with
+  // a bigger ratio, but once resolution gets above around 1024 this needs
+  // to be compatible with the CFL limit.
+  //
+  // With really steep initial data there can be minor Gibbs oscillation
+  // effects, but even at crazy resolutions (65k) these are small provided
+  // the CFL limit is met.
+  bool output(true);
+  int nreports(5);
+
+  SerialEnv env(&argc, &argv, 1, 1, 1);
+
+  DataArgs data_args(nx, ny, nz, xmin, xmax, ymin, ymax, zmin, zmax, endTime);
+  data_args.sCfl(cfl);
+  data_args.sNg(Ng);
+  data_args.gamma = 5.0/3.0;
+  const std::vector<double> toy_params           { {1.0e-15, 1.0e-1,  5.0e-2, 1.0e-1,  1.0e-15, 1.0e-1} };
+  const std::vector<std::string> toy_param_names = {"kappa", "tau_q", "zeta", "tau_Pi", "eta", "tau_pi"};
+  const int n_toy_params(6);
+  data_args.sOptionalSimArgs(toy_params, toy_param_names, n_toy_params);
+
+  Data data(data_args, &env);
+
+  // Choose particulars of simulation
+  IS model(&data);
+
+  Weno3 weno(&data);
+
+  FVS fluxMethod(&data, &weno, &model);
+
+  Outflow bcs(&data);
+  // Periodic bcs(&data);
+
+  Simulation sim(&data, &env);
+
+  // Smeared_Shocktube_1D_Para init(&data);
+  Shocktube init(&data); //direction given by second arg (int)
+  // Blob2dToyQ init(&data);
+  //ISKHInstabilitySingleFluid init(&data, 1);
+  //Shocktube_Chab21 init(&data);  
+
+  // RKSplit timeInt(&data, &model, &bcs, &fluxMethod);
+  // BackwardsRK2 timeInt(&data, &model, &bcs, &fluxMethod);
+  // SSP2 timeInt(&data, &model, &bcs, &fluxMethod);
+  RK2B timeInt(&data, &model, &bcs, &fluxMethod);
+
+  SerialSaveDataHDF5 save(&data, &env, "1d/bulk/data_serial_TIx_0", SerialSaveDataHDF5::OUTPUT_ALL);
+
+  // Now objects have been created, set up the simulation
+  sim.set(&init, &model, &timeInt, &bcs, &fluxMethod, &save);
+
+  save.saveAll();
+
+  for (int n(0); n<nreports; n++) {
+    data.endTime = (n+1)*endTime/(nreports);
+    SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/bulk/data_serial_TIx_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
+    sim.evolve(output);
+    save_in_loop.saveAll();
+  }
+
+  return 0;
+
+}
