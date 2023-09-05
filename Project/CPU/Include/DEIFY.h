fc0fd293ae5d9538ce5449bd13b31b6c88a72bcc
MarcusHatton <74266732+MarcusHatton@users.noreply.github.com>
2023-09-05 16:05:19 +0100

    using new Grad function to do derivs instead of simple finite differencing... for nearly all variables now, only missing time derivs

--- Project/CPU/Include/DEIFY.h
+++ Project/CPU/Include/DEIFY.h
@@ -79,29 +79,6 @@ class DEIFY : public ModelExtension
     template<typename T>
     T cube(T x) { return ((x) * (x) * (x)); }
 
-    // // First and second order "minmod" functions for slope-limiting
-    // double minmodGradFO(double im1, double i, double ip1, double dX) {
-
-    //   double FDGrad = (-1.0*i + 1*ip1)/dX;
-    //   double BDGrad = (1.0*i - 1*im1)/dX;
-    //   if ( (FDGrad < 0 && BDGrad > 0) || (FDGrad > 0 && BDGrad < 0) ) {
-    //     return 0;
-    //   } else {
-    //     return abs(FDGrad) < abs(BDGrad) ? FDGrad : BDGrad;
-    //   }
-    // }
-
-    // double minmodGradSO(double im2, double im1, double i, double ip1, double ip2, double dX) {
-      
-    //   double FDGrad = (-1.5*i + 2*ip1 - 0.5*ip2)/dX;
-    //   double BDGrad = (1.5*i - 2*im1 + 0.5*im2)/dX;
-    //   if ( (FDGrad < 0 && BDGrad > 0) || (FDGrad > 0 && BDGrad < 0) ) {
-    //     return 0;
-    //   } else {
-    //     return abs(FDGrad) < abs(BDGrad) ? FDGrad : BDGrad;
-    //   }
-    // }
-    
     // enums to save looking up numbering of C/P/As when using ID accessor.
     enum Cons { D, S1, S2, S3, Tau };
     enum Prims { v1, v2, v3, p, rho, n, q1, q2, q3, Pi, pi11, pi12, pi13, pi22, pi23, pi33 };
