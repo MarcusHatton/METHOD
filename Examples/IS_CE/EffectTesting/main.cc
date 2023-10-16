ee4ff34d1ad90941056f1aaf3a84a785b1114016
Marcus J Hatton <mjh1n20@soton.ac.uk>
2023-10-16 15:22:44 +0100

    main and submitMe changes

--- Examples/IS_CE/EffectTesting/main.cc
+++ Examples/IS_CE/EffectTesting/main.cc
@@ -2,13 +2,14 @@
 #include "simData.h"
 #include "simulation.h"
 #include "initFunc.h"
+//#include "IS.h"
 #include "ISCE.h"
+#include "DEIFY.h"
 #include "boundaryConds.h"
 // #include "rkSplit.h"
 // #include "backwardsRK.h"
 #include "RKPlus.h"
 // #include "SSP2.h"
-#include "DEIFY.h"
 #include "fluxVectorSplitting.h"
 #include "serialEnv.h"
 #include "serialSaveDataHDF5.h"
@@ -20,18 +21,20 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-  float taus[] = {1e-2, 1e-3, 1e-4 };
+
+  float taus[] = {1e-3, 1e-2, 1e-1, 1.0 };
   float tau = 0;
 
   for(int i=0; i<4; i++) {
     tau = taus[i];
     cout << tau << std::endl;
-    std::string dirpath = "./1d/bulk/taus/"+std::to_string(tau);
+    //std::string dirpath = "./1d/all/stillshock/taus/"+std::to_string(tau);
+    std::string dirpath = "../../../../../../scratch/mjh1n20/StillShock/tau_scaling/ISCE/"+std::to_string(tau);
     mkdir(dirpath.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
   
   // Set up domain
   int Ng(4);
-  int nx(1200);
+  int nx(4000);
   //if(argc>=2) { nx=atoi(argv[1]); }
   int ny(0);
   int nz(0);
@@ -42,7 +45,7 @@ int main(int argc, char *argv[]) {
   double zmin(0.0);
   double zmax(1.0);
   double endTime(2.0);
-  double cfl(0.4);
+  double cfl(0.1);
   // double gamma(0.001);
   // double sigma(0.001);
   // These parameters work with IMEX SSP2; given that tau_q << dt,
@@ -66,7 +69,7 @@ int main(int argc, char *argv[]) {
   data_args.sCfl(cfl);
   data_args.sNg(Ng);
   data_args.gamma = 5.0/3.0;
-  const std::vector<double> toy_params           { {1.0e-15, 1e-1,  5.0e-2, tau, 1.0e-15, 1e-1} };
+  const std::vector<double> toy_params           { {1.0e-4, tau,  1.0e-2, tau, 1.0e-3, tau} };
   const std::vector<std::string> toy_param_names = {"kappa", "tau_q", "zeta", "tau_Pi", "eta", "tau_pi"};
   const int n_toy_params(6);
   data_args.sOptionalSimArgs(toy_params, toy_param_names, n_toy_params);
@@ -99,7 +102,8 @@ int main(int argc, char *argv[]) {
   // SSP2 timeInt(&data, &model, &bcs, &fluxMethod);
   RK2B timeInt(&data, &model, &bcs, &fluxMethod, &modelExtension);
 
-  SerialSaveDataHDF5 save(&data, &env, "1d/bulk/taus/"+std::to_string(tau)+"/ds_0", SerialSaveDataHDF5::OUTPUT_ALL);
+  //SerialSaveDataHDF5 save(&data, &env, "1d/all/stillshock/taus/"+std::to_string(tau)+"/ds_0", SerialSaveDataHDF5::OUTPUT_ALL);
+  SerialSaveDataHDF5 save(&data, &env, dirpath+"/ds_0", SerialSaveDataHDF5::OUTPUT_ALL);
 
   // Now objects have been created, set up the simulation
   sim.set(&init, &model, &timeInt, &bcs, &fluxMethod, &save);
@@ -108,7 +112,8 @@ int main(int argc, char *argv[]) {
 
   for (int n(0); n<nreports; n++) {
     data.endTime = (n+1)*endTime/(nreports);
-    SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/bulk/taus/"+std::to_string(tau)+"/ds_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
+    //SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/all/stillshock/taus/"+std::to_string(tau)+"/ds_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
+    SerialSaveDataHDF5 save_in_loop(&data, &env, dirpath+"/ds_"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
     sim.evolve(output);
     save_in_loop.saveAll();
   }
